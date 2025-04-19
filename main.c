2520fcc872dff630fe0dc3a3b226bd3dedaab806
salaheddine <salaheddineh7@gmail.com>
2025-04-19 12:27:37 +0100

    last_modf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: salah <salah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 19:16:01 by salhali           #+#    #+#             */
-/*   Updated: 2025/04/19 12:06:04 by salah            ###   ########.fr       */
+/*   Updated: 2025/04/19 12:27:19 by salah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		printf("Invalid arguments\n");
 		return (1);
 	}
-    print_data(&data);
+    // print_data(&data);
 	if (init_data(&data))
 	{
 		printf("Initialization failed\n");
@@ -42,72 +42,6 @@ int main(int argc, char **argv)
 	cleanup(&data);
 	return (0);
 }
-
-// int main(int argc, char **argv)
-// {
-//     t_data data;
-//     // t_philo philo;
-//     pthread_t monitor_thread;
-//     int i;
-//     // int j;
-
-//     // j = 1;
-
-//     // ft_memset(&data, 0, sizeof(t_data));
-//     // while(j < argc)
-//     // {
-//     //     printf("argv[%d] = %s\n", j, argv[j]);
-//     //     if(is_valid_number(argv[j]) == 0)
-//     //     {
-//     //         printf("Invalid arguments (function : is_valid_number) N/23 \n");
-//     //         return (1);
-//     //     }
-//     //     j++;
-//     // }
-//     ft_memset(&data, 0, sizeof(t_data));
-//     if (parse_args(argc, argv, &data))
-//     {
-//         printf("Invalid arguments\n");
-//         return (1);
-//     }
-
-//     ///////////////////////////////////////
-//     // printf("%d\n", data.num_philos);
-//     // printf("%ld\n", data.time_to_die);
-//     // printf("%ld\n", data.time_to_eat);
-//     // printf("%ld\n", data.time_to_sleep);
-//     // printf("%d\n", data.meals_required);
-//     ////////////////////////////////////////
-
-//     if (init_data(&data))
-//     {
-//         printf("Initialization failed\n");
-//         return (1);
-//     }
-//     printf("%d\n", data.num_philos);
-//     printf("%ld\n", data.time_to_die);
-//     printf("%ld\n", data.time_to_eat);
-//     printf("%ld\n", data.time_to_sleep);
-//     printf("%d\n", data.meals_required);
-//     // printf("ID : %d\n", philo.id);
-//     // printf("last_meal : %ld\n", philo.last_meal);
-//     // printf("meals_eaten : %d\n", philo.meals_eaten);
-
-//     // i = 0;
-//     // while(i < data.num_philos)
-//     // {
-//     //     pthread_create(&data.philos[i].thread, NULL, philo_routine, &data.philos[i]);
-//     // }
-//     // pthread_create(&monitor_thread, NULL, (void *)monitor, &data);
-//     // i = 0;
-//     // while (i < data.num_philos)
-//     // {
-//     //     pthread_join(data.philos[i].thread, NULL);
-//     // }
-//     // pthread_join(monitor_thread, NULL);
-//     cleanup(&data);
-//     return (0);
-// }
 
 // ./philo 5 800 200 200 7
 
