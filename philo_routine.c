cf6b6008fe8e2b8490c9c3d48a9c02105d9c05c5
Salah Eddine Hali <salhali@e2r6p2.1337.ma>
2025-05-15 20:50:47 +0100

    Done

--- philo_routine.c
+++ philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 11:33:49 by salhali           #+#    #+#             */
-/*   Updated: 2025/05/14 18:47:12 by salhali          ###   ########.fr       */
+/*   Updated: 2025/05/15 19:48:58 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,26 @@ void	take_forks(t_philo *philo)
     if (philo->id % 2 == 0)
     {
     	pthread_mutex_lock(philo->right_fork);
-    	print_state(philo, "has taken a fork right [1]");
+    	print_state(philo, "has taken a fork");
     	pthread_mutex_lock(philo->left_fork);
-    	print_state(philo, "has taken a fork left");
+    	print_state(philo, "has taken a fork");
     }
     else
 	{
     	pthread_mutex_lock(philo->left_fork);
-    	print_state(philo, "has taken a fork left [2]");
+    	print_state(philo, "has taken a fork");
     	pthread_mutex_lock(philo->right_fork);
-    	print_state(philo, "has taken a fork right");
+    	print_state(philo, "has taken a fork");
     }
 }
 
 void	thinking(t_philo *philo)
 {
-	print_state(philo, "is thinking");
+	// long long result;
+	
+	// result = philo->data->time_to_eat + philo->data->time_to_sleep ;
+	// if(result > philo->data->time_to_die)
+		print_state(philo, "is thinking");
 }
 
 int	check_death_or_meal(t_philo *philo)
@@ -70,8 +74,6 @@ void	*philo_routine(void *arg)
 	pthread_mutex_lock(&philo->data->death_mutex);
 	philo->last_meal = get_time();
 	pthread_mutex_unlock(&philo->data->death_mutex);
-	if(philo->data->num_philos == 1)
-		pthread_create(&philo[0].thread, NULL, &philo_routine_single, &philo->data);
 	if (philo->id % 2 == 0)
 		precise_sleep(10);
 	while (1)
@@ -81,7 +83,6 @@ void	*philo_routine(void *arg)
 		take_forks(philo);
 		eat_sleeping(philo);
 		thinking(philo);
-		// usleep(10);
 	}
 	return (NULL);
 }
